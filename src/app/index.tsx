7e11b30dd7c5f1985e9d3a7e780fbd10bf6aeaff
hattab <admin@kremlin.ru>
2024-10-18 00:02:05 +0300

    horror movie

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -27,7 +27,7 @@ export const App = () => {
 					type='secondary'
 					style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
 					<BurgerIcon type='primary' />
-					<span style={{ color: 'white' }}>Какое безобразие</span>
+					<span style={{ color: 'white' }}>Какое безобразие и безответственность</span>
 				</Button>
 				<Button
 					htmlType='button'
