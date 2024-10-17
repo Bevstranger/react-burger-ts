2fa4966119af8f1ac211e007cbf2e43f59bafdcc
Evgen <kapranov.evgeniy@gmail.com>
2024-10-17 23:47:33 +0300

    init cha

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,7 @@ import { useState } from 'react';
 import s from './app.module.scss';
 import reactLogo from './assets/react.svg';
 import { ReactComponent as TypescriptLogo } from './assets/typescript.svg';
-import {
-	BurgerIcon,
-	Button,
-	ListIcon,
-	Logo,
-	ProfileIcon,
-} from '@ya.praktikum/react-developer-burger-ui-components';
+import {} from '@ya.praktikum/react-developer-burger-ui-components';
 
 export const App = () => {
 	return (
