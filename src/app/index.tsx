e54eea24949df5980806ffa1d348843da5e298e5
Evgen <kapranov.evgeniy@gmail.com>
2024-10-17 23:22:13 +0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,56 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import s from './app.module.scss';
+import reactLogo from './assets/react.svg';
+import { ReactComponent as TypescriptLogo } from './assets/typescript.svg';
+import {
+	BurgerIcon,
+	Button,
+	ListIcon,
+	Logo,
+	ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
+export const App = () => {
+	return (
+		<>
+			<nav
+				style={{
+					height: 50,
+					color: 'white',
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignContent: 'center',
+				}}>
+				<Button
+					htmlType='button'
+					type='secondary'
+					style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+					<BurgerIcon type='primary' />
+					<span style={{ color: 'white' }}>Конструктор</span>
+				</Button>
+				<Button
+					htmlType='button'
+					type='secondary'
+					style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+					<ListIcon type='primary' />
+					<span style={{ color: 'white', whiteSpace: 'nowrap' }}>
+						Лента заказов
+					</span>
+				</Button>
+
+				<Logo />
+
+				<Button
+					htmlType='button'
+					type='secondary'
+					style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+					<ProfileIcon type='primary' />
+					<span style={{ color: 'white', whiteSpace: 'nowrap' }}>
+						Личный кабинет
+					</span>
+				</Button>
+			</nav>
+		</>
+	);
+};
